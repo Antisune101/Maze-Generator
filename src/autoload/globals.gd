3c2bb59c339cfaeb991de8cdee03bad915959324
Antisune101 <ewanbester72@gmail.com>
2025-10-27 19:05:48 +0200

    Decoupled maze from main scene

---
+++ src/autoload/globals.gd
@@ -0,0 +1,6 @@
+extends Node
+
+signal maze_parameters_updated
+
+const WALL_WIDTH = 2
+const PADDING = 8
--- src/maze/maze.gd
+++ src/maze/maze.gd
@@ -1,12 +1,10 @@
-extends Node2D
-
+class_name Maze extends Node2D
 
 @export var maze_node: Node2D
 @export var sprite: Sprite2D
 
-const CELL_SIZE = 32
-const GRID_SIZE = Vector2i(20, 20)
-const LINE_WIDTH = 2
+var grid_size = Vector2i(10, 10)
+var cell_size = 32
 
 var v_walls := {}
 var h_walls := {}
@@ -14,23 +12,36 @@ var outer_walls: Array[Line2D] = []
 
 
 func _ready() -> void:
-	# Center grid
-	var center = get_viewport_rect().get_center()
-	var grid_with = GRID_SIZE.x * CELL_SIZE + LINE_WIDTH
-	var grid_height = GRID_SIZE.y * CELL_SIZE + LINE_WIDTH
-	maze_node.global_position.x = center.x - grid_with / 2.0
-	maze_node.global_position.y = center.y - grid_height / 2.0
+	generate_grid()
+	generate_maze()
+	Globals.maze_parameters_updated.connect(generate_grid)
+
+
+func generate_grid() -> void:
+	for wall in maze_node.get_children():
+		wall.queue_free()
+	v_walls.clear()
+	h_walls.clear()
+	outer_walls.clear()
 	
+	
+	# Center grid
+	var max_width = get_viewport_rect().size.y - Globals.PADDING * 2
+	cell_size = max_width / max(grid_size.x, grid_size.y) 
+	var left = get_viewport_rect().size.x
+	maze_node.global_position = Vector2(left - (max_width + Globals.PADDING), Globals.PADDING)
+	sprite.global_position = maze_node.global_position
+	sprite.visible = false
 	# Generate walls and store them in h_walls and v_walls
-	for y in GRID_SIZE.y + 1:
-		for x in GRID_SIZE.y:
-			if y == 0 or y == GRID_SIZE.y:
+	for y in grid_size.y + 1:
+		for x in grid_size.y:
+			if y == 0 or y == grid_size.y:
 				outer_walls.append(build_wall(x, y, x+1, y))
 			else:
 				h_walls[Vector2i(x, y-1)] = build_wall(x, y, x+1, y)
-	for y in GRID_SIZE.y:
-		for x in GRID_SIZE.x + 1:
-			if x == 0 or x == GRID_SIZE.x:
+	for y in grid_size.y:
+		for x in grid_size.x + 1:
+			if x == 0 or x == grid_size.x:
 				outer_walls.append(build_wall(x, y, x, y+1))
 			else:
 				v_walls[Vector2i(x-1, y)] = build_wall(x, y, x, y+1)
@@ -41,28 +52,17 @@ func _ready() -> void:
 		wall.width = 4
 
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
-		generate_maze()
-
-
-func init_grid() -> void:
-	pass
-
-
-func reset_grid() -> void:
-	pass
-
 func build_wall(x1, y1, x2, y2) -> Line2D:
 	var wall = Line2D.new()
-	wall.width = 2
+	wall.width = Globals.WALL_WIDTH
 	maze_node.add_child(wall)
-	wall.add_point(vec_to_position(x1, y1))
-	wall.add_point(vec_to_position(x2, y2))
+	wall.add_point(vec_to_position(Vector2i(x1, y1)))
+	wall.add_point(vec_to_position(Vector2i(x2, y2)))
 	return wall
 
-func vec_to_position(x,y) -> Vector2:
-	return Vector2(x, y) * CELL_SIZE
+
+func vec_to_position(vec: Vector2i) -> Vector2:
+	return Vector2(vec.x, vec.y) * cell_size
 
 
 func delete_v_wall(x, y) -> void:
@@ -78,8 +78,9 @@ var untraversed_cells: Array[Vector2i] = []
 var travel_history: Array[Vector2i] = []
 
 func generate_maze() -> void:
-	for y in GRID_SIZE.y:
-		for x in GRID_SIZE.x:
+	sprite.visible = true
+	for y in grid_size.y:
+		for x in grid_size.x:
 			cells.append(Vector2i(x, y))
 	untraversed_cells = cells
 	var starting_cell = cells.pick_random()
@@ -88,7 +89,7 @@ func generate_maze() -> void:
 
 func visit_cell(cell: Vector2i) -> void:
 	await get_tree().create_timer(.05).timeout
-	sprite.position = cell * CELL_SIZE
+	sprite.position = cell * cell_size
 	sprite.self_modulate = Color.GREEN
 	
 	travel_history.append(cell)
@@ -106,7 +107,7 @@ func visit_cell(cell: Vector2i) -> void:
 
 func backtrack(cell: Vector2i) -> void:
 	await get_tree().create_timer(.05).timeout
-	sprite.position = cell * CELL_SIZE
+	sprite.position = cell * cell_size
 	sprite.self_modulate = Color.RED
 	travel_history.pop_back()
 	if is_cell_exhausted(cell):
@@ -139,6 +140,6 @@ func find_untraversed_neighbors(cell: Vector2i) -> Array[Vector2i]:
 		Vector2i(x, y+1),
 		Vector2i(x, y-1),
 	]
-	n = n.filter(func(c): return c.x >= 0 && c.x < GRID_SIZE.x && c.y >= 0 && c.y < GRID_SIZE.y)
+	n = n.filter(func(c): return c.x >= 0 && c.x < grid_size.x && c.y >= 0 && c.y < grid_size.y)
 	n = n.filter(func(c): return untraversed_cells.has(c))
 	return n
